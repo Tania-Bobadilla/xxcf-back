9510d9e3753cece817e1e0331bddb91d993aac26
tania.b <carolinaortz1@gmail.com>
2024-05-07 15:15:51 -0400

    tablas

--- database/db.sql
+++ database/db.sql
@@ -5,3 +5,12 @@ CREATE TABLE form (
     title VARCHAR (255) UNIQUE,
     description VARCHAR (255) UNIQUE
 );
+
+CREATE TABLE public.form (
+	user_id int NOT NULL,
+	nombres varchar NOT NULL,
+	direccion varchar NOT NULL,
+	pais varchar NOT NULL,
+	ciudad varchar NOT NULL,
+	CONSTRAINT form_pk PRIMARY KEY (user_id)
+);
