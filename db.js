cc73d8e3114848eb5488dc11061069a796c73bbb
tania.b <carolinaortz1@gmail.com>
2024-05-14 15:09:45 -0400

    ok

--- db.js
+++ db.js
@@ -1,37 +1,37 @@
-const {Pool} = require ("pg");
-require("dotenv").config();
-const user = process.env.USER
-const password = process.env.PASSWORD
-const host = process.env.HOST
-const port = process.env.PORT
-const database = process.env.DATABASE
+// const {Pool} = require ("pg");
+// require("dotenv").config();
+// const user = process.env.USER
+// const password = process.env.PASSWORD
+// const host = process.env.HOST
+// const port = process.env.PORT
+// const database = process.env.DATABASE
 
 
-const pool = new Pool ({
+// const pool = new Pool ({
     
-    user: user,
-    password: password,
-    host: host, 
-    port: port, 
-    database: database,
+//     user: user,
+//     password: password,
+//     host: host, 
+//     port: port, 
+//     database: database,
     // ssl: true
     // ssl: {
     //     rejectUnauthorized: false
     // }
-});
+// });
 
  
 //puedo hacer esto con el try catch
-pool.connect((err) => {
-    if (err) {
-        console.error("error de conexion", err.stack)
-    } else {
-        console.log("conectado!!")
-    }
-})
+// pool.connect((err) => {
+//     if (err) {
+//         console.error("error de conexion", err.stack)
+//     } else {
+//         console.log("conectado!!")
+//     }
+// })
 
 
 
     
 
-module.exports = pool;
+// module.exports = pool;
