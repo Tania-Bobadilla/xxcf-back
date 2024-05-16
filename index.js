80adff3e1180adcd24f8c6439f41f8294d134ee7
tania.b <carolinaortz1@gmail.com>
2024-05-16 12:33:45 -0400

    ok

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const formRoutes = require("./routes/formRoutes");
+// const formRoutes = require("./routes/formRoutes");
 
 
 // app.use(cors());
@@ -9,15 +9,23 @@ const cors = require("cors")
 //instanciar express (llamar a todas las funciones de express)
 const app = express();
 
-require("./db")
+// require("./db")
 
 app.use(cors());
 
 app.use(express.json())
 
-app.use(formRoutes)
+// app.use(formRoutes)
 
+// const formRouter = Router();
 
+app.get("/form", async (req, res) => {
+    try {
+        res.send("retornar lista de tareas")
+    } catch (error) {
+        res.send(error.message)
+    }
+})
 
 // require("dotenv").config();
 // const port = process.env.PORT;
@@ -29,5 +37,7 @@ app.use(formRoutes)
 // app.use(cors());
 
 //antes 4000
-app.listen(8080, () => console.log("servidor levantado en puerto 4000"))
+app.listen(8080, () => console.log("servidor levantado en puerto 8080"))
 
+//en terminal de putty/linux, para detener puerto en uso
+// npx kill-port 3000
