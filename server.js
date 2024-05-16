27e6c704ff6db214da08f9c6e40b1308f02051bf
tania.b <carolinaortz1@gmail.com>
2024-05-16 14:42:16 -0400

    ok

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const cors = require('cors')
 const express = require('express')
 //por que esta escrito asi?
-const port = process.env.PORT||8080
+// const port = process.env.PORT||8080
 const app = express()
 
 app.use(express.json(), cors())
 
 
 //ver si le tengo que quitar el async
-app.get("/", async (req, res) => {
+app.get("/uno", async (req, res) => {
     try {
         res.send("retornar lista de tareas")
     } catch (error) {
@@ -16,4 +16,4 @@ app.get("/", async (req, res) => {
     }
 });
 
-app.listen(port, () => console.log("servidor se levantado en puerto 8080!"))
+app.listen(8080, () => console.log("servidor se levantado en puerto 8080!"))
