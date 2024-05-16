4c1e6c18a63efac4a5317dd6488cbe4eca718fa7
tania.b <carolinaortz1@gmail.com>
2024-05-16 14:31:35 -0400

    ok

--- index.js
+++ index.js
@@ -1,43 +1,23 @@
 const express = require("express");
-// const formRoutes = require("./routes/formRoutes");
-
-
-// app.use(cors());
-
+const formRoutes = require("./routes/formRoutes");
 const cors = require("cors")
 
 //instanciar express (llamar a todas las funciones de express)
 const app = express();
 
-// require("./db")
-
-// app.use(cors());
+require("./db")
 
 app.use(express.json(), cors())
 
-// app.use(formRoutes)
+app.use(formRoutes)
 
-// const formRouter = Router();
-
-app.get("/", async (req, res) => {
-    try {
-        res.send("retornar lista de tareas")
-    } catch (error) {
-        res.send(error.message)
-    }
-})
 
 // require("dotenv").config();
 // const port = process.env.PORT;
 
-//middlewares
-
-
-
-// app.use(cors());
 
 //antes 4000
-app.listen(4000, () => console.log("servidor levantado en puerto 8080"))
+app.listen(8080, () => console.log("servidor levantado en puerto 8080"))
 
 //en terminal de putty/linux, para detener puerto en uso
 // npx kill-port 3000
---
+++ server.js
@@ -0,0 +1,19 @@
+const cors = require('cors')
+const express = require('express')
+//por que esta escrito asi?
+const port = process.env.PORT||8080
+const app = express()
+
+app.use(express.json(), cors())
+
+
+//ver si le tengo que quitar el async
+app.get("/", async (req, res) => {
+    try {
+        res.send("retornar lista de tareas")
+    } catch (error) {
+        res.send(error.message)
+    }
+});
+
+app.listen(port, () => console.log("servidor levantado en puerto 8080"))
